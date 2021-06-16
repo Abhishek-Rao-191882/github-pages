3341c7235b5458cee9c4fa03b2aa6a406ecd7e4a
Abhishek Rao <arao8732@gmail.com>
2021-06-16 15:24:25 +0530

    updated

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ var menuBtn = document.getElementById("menuBtn");
 var sidenav = document.getElementById("sidenav");
 
 sidenav.style.right = "-250px";
+
 menuBtn.onclick = function () {
 	if (sidenav.style.right == "-250px") {
 		sidenav.style.right = "0"
